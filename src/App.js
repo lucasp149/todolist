a0677fad95a7cea438e55879c9d996499bf5ee01
lucasp149 <lucasp149@gmail.com>
2022-07-08 16:32:23 -0300

    dfs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import TodoApp from "../src/components/todoApp"
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
--- src/components/todoApp.js
+++ src/components/todoApp.js
@@ -1,33 +1,49 @@
 import { useState } from "react";
+import React from "react";
 
 
 
 export default function TodoApp() {
 
     const [title, setTitle] = useState("hola");
+    const [todos, setTodos] = useState([]);
 
-    function handleClick(e) {
-        e.preventDefault();
-        setTitle("Marcos");
-    }
+   
 
-    function handleChange(e){
+    function handleChange(e) {
         var value = e.target.value;
         setTitle(value);
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        const newTodo = {
+            id: crypto.randomUUID(),
+            title: title,
+            completed: false
+        }
+
+        let temp = [...todos];
+        temp.unshift(newTodo);
+
+        setTodos(temp);
     }
 
     return (
         <div className="todoContainer">
 
-            <form className="todoForm">
-                <input onchange={handleChange} className="todoInput" defaultValue= {title} />
-                <input onclick={handleClick}  type="submit" className="buttonCreate" value="create todo" />
+            <form className="todoForm" onSubmit={handleSubmit}>
+                <input onChange={handleChange} className="todoInput" value={title} />
+                <input onClick={handleSubmit} type="submit" className="buttonCreate" value="create todo" />
                 {title}
             </form>
 
 
             <div className="generalContainer">
-
+                {
+                    todos.map(item => (
+                        <div> {item.title}</div>
+                    ))}
             </div>
 
         </div>
